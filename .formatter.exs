65c2236412f581851cb7abad7d43998da716cd29
Tyler Young <tyler@tylerayoung.com>
2023-03-29 10:28:16 -0500

    Export locals without parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,8 @@
+locals_without_parens = [union_type: 1]
+
 [
   line_length: 120,
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [union_type: 1]
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
